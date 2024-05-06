a9e5a26995882dcc0b851d361c171f17c8b49c04
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-06 10:31:34 +0700

    object baru 1

--- pemdas_pointer/pemdas_pointer.cpp
+++ pemdas_pointer/pemdas_pointer.cpp
@@ -7,4 +7,15 @@ using namespace std;
 class mahasiswa {
 public:
     int nim;
+    void showNim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main() {
+    mahasiswa mhs{ 1 };
+    mhs.showNim();
+
+    
+}
 
