ddad8519e6775f9111b02b3302f73f3c16f63a38
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-06 10:48:21 +0700

    membuat class siswa dan orang

--- FriendClassSatu/FriendClassSatu.cpp
+++ FriendClassSatu/FriendClassSatu.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang {
+private:
+    string nama;
+
+public:
+    void setnama(string pNama);
+    friend class siswa;
+};
+
+
+
