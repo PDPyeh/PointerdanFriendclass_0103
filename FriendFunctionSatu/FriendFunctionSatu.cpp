2794e62e063f2323eac4c8acc4e2506fead66a55
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-06 10:46:19 +0700

    delete

--- FriendFunctionSatu/FriendFunctionSatu.cpp
+++ FriendFunctionSatu/FriendFunctionSatu.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+    int nim;
+    void showNim() {
+        cout << "no Induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+    mahasiswa* mhs = new mahasiswa{ 1 };
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
+
